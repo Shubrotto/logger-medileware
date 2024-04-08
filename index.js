f9ff51769e166190bd43bdfa87389c2d237566a0
Shubrotto Kumar Hawlader <skshubroto@gmail.com>
2024-04-08 21:53:41 +0600

    validatorjs

--- index.js
+++ index.js
@@ -1,16 +1,43 @@
 const koa = require("koa");
+const bodyParser = require("koa-body-parser");
+const Validator = require("validatorjs");
+
 const app = new koa();
+
 const port = 3000;
 
+app.use(bodyParser());
+
+const validRules = {
+  Id: `id is required | ${Validator.message}`,
+  Name: `name is required`,
+  Email: `email is required`,
+};
+
 app.use(async (ctx, next) => {
-  const startMs = Date.now();
-  await next();
-  console.log(`${Date.now() - startMs} ms`);
-  console.log(ctx.message);
+  const data = ctx.request.body;
+  console.log("response Data", data);
+
+  const validation = new Validator(data, validRules);
+  console.log(validation.passes());
+  if (validation.passes()) {
+    ctx.body = `
+    Data is successfully saved! 
+    `;
+    await next();
+  } else {
+    ctx.response.status = 400;
+    console.log(ctx.response.status);
+    ctx.body = {
+      message: "validation failed",
+      Error: validation.errors.all(),
+    };
+    validation.errors.first("id");
+  }
 });
 
 app.use(async (ctx, next) => {
-  ctx.body = "Hello Koa!, Now you are worked";
+  ctx.status = 200;
 });
 
 app.listen(port, () => {
